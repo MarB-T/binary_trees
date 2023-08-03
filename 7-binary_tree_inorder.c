77950d6827026784be72b6e7cc24f28654ea985c
MarB-T <danmarbet@gmail.com>
2023-08-03 17:47:44 +0300

    Binary_tree_inorder correction

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,6 +8,8 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if ((func == NULL) || (tree == NULL))
+		return;
 	if (tree->left != NULL)
 		binary_tree_inorder(tree->left, func);
 	func(tree->n);
