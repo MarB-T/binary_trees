18109760a1b83d2fcccd0a50058a396ca6725291
MarB-T <danmarbet@gmail.com>
2023-08-05 05:55:10 +0300

    Tree traversal using level order

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+void print_level(const binary_tree_t *tree, int level, void (*func)(int));
+
+/**
+ * binary_tree_levelorder - goes through a binary tree using level orde traversal
+ * @tree: pointer to the tree to traverse
+ * @func: pointer to function to handle node data
+ * Return: nothing
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t height, i;
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	height = binary_tree_height(tree);
+
+	for (i = 1; i <= height + 1; i++)
+		print_level(tree, i, func);
+}
+
+void print_level(const binary_tree_t *tree, int level, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	if (level == 1)
+		func(tree->n);
+	else if (level > 1)
+	{
+		print_level(tree->left, level - 1, func);
+		print_level(tree->right, level - 1, func);
+	}
+}
